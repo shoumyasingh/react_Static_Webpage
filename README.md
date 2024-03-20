7c6f07d059656c4bc45a4703789c5330250394e9
Shoumya Singh <shoumya25@gmail.com>
2024-03-20 13:27:04 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# React_Static_Webpage
+ React,Javascript,HTML,CSS
---
+++ index.js
@@ -0,0 +1,19 @@
+
+const samplePage = (
+    <div>
+      <img src="./react-logo.png" width="40px" alt="React Logo."/>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+  )
+ 
+ReactDOM.render(samplePage, document.getElementById("root"))
+  // Select the div with ID "root" and append the JSX
+//document.getElementById("root").append(JSON.stringify(samplePage));
+  
