82b0da0bccb62256321292c2f2c46948286f339b
Shoumya Singh <shoumya25@gmail.com>
2024-03-21 20:58:39 -0700

    App uploaded

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
-const samplePage = (
-    <div>
-      <img src="./react-logo.png" width="40px" alt="React Logo."/>
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-  )
- 
-ReactDOM.render(samplePage, document.getElementById("root"))
-  // Select the div with ID "root" and append the JSX
-//document.getElementById("root").append(JSON.stringify(samplePage));
-  
+ReactDOM.render(<App />, document.getElementById("root"))
